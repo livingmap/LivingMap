a87e8b6a599308af279aa1f2b261713828ea30ad
SohaAhmed <soha.ahmed@livingmap.com>
2022-09-21 17:29:29 +0100

    update to 11.3.0

---
+++ Sources/LivingMapSDK.xcframework/ios-arm64/LivingMapSDK.framework/Headers/CPLHeadingEstimateType.h
@@ -0,0 +1,15 @@
+//
+//  CPLHeadingEstimateType.h
+//  LivingMapSDK
+//
+//  Copyright © 2021 LivingMap. All rights reserved.
+//
+
+typedef enum {
+    HET_NONE,
+    HET_MAG,
+    HET_CPL,
+    HET_DEVICE
+    
+} CPLHeadingEstimateType;
+
