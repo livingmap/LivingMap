e1ac63dcd42b2da9cd4feec83ce3434b9cbd7d8f
dblake78 <dblake78@aol.com>
2023-07-24 11:06:41 +0100

    Add xcframeworks for 11.4.6

---
+++ Sources/LivingMapSDK.xcframework/ios-arm64/LivingMapSDK.framework/Headers/CalibrationType.h
@@ -0,0 +1,13 @@
+//
+//  CalibrationType.h
+//  LivingMapSDK
+//
+//  Copyright © 2022 LivingMap. All rights reserved.
+//
+
+typedef enum {
+    NONE,
+    ELLIPSOID_FITTING,
+    DYNAMIC
+} CalibrationType;
+
